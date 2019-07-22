f2e06a481e7693f9be38cb451e2827114abf5d67
Mike Dollar <mikedollardata@gmail.com>
2019-07-22 16:00:17 -0400

    saving lab question work and new data table

--- mikedollar/questions.R
+++ mikedollar/questions.R
@@ -42,8 +42,61 @@ df_ret = merge(returns, df, by='order_id', all = TRUE)
 
 df_ret$returned[is.na(df_ret$returned)] = 'No'
 
-# first get the total profit 
-loss = df_ret %>% 
-  filter(., returned == 'yes') %>% 
+# this is how much is lost each year due to returns:
+View(df_ret %>% 
+  filter(., returned == 'Yes', profit < 0 ) %>% 
   group_by(., year = year(order_date)) %>% 
-  summarise(., sum(sales))
+  summarise(., sum(profit)))
+
+#how many customers returned more than 1 and 5 times?
+# 1061 returned items at least once
+# 80 returned items at least 5 times
+df_ret %>% 
+  filter(.,returned == 'Yes') %>% 
+  group_by(., customer_id) %>% 
+  summarise(., total_returns = n()) %>% 
+  count(total_returns >= 5)
+
+
+# which regions are more likely to return orders?
+
+df_ret %>%
+  filter(., returned == 'Yes') %>%
+  group_by(., region.1) %>%
+  summarise(., total_returns = n())
+
+returns_yes = df_ret %>%
+  filter(., returned == 'Yes')
+
+ggplot(df_ret %>% filter(., returned == 'Yes'), aes(x=region.1))+
+  geom_histogram(stat='count')
+
+# looks like Central America and western europe
+
+# which categories of products are more likely to be returned?
+
+ggplot(df_ret %>% filter(., returned == 'Yes'), aes(x=category))+
+  geom_histogram(stat='count')
+
+# office supplies
+
+# create new independent variable process_time_days
+df_ret = df_ret %>% 
+  mutate(., process_time_days = (ship_date - order_date)/(24*3600))
+
+# create new independent variable for frequency of item returned
+df_ret %>% 
+  mutate(., times_returned = )
+# start by creating table grouped by product_id and summing occurences when returned == Yes
+
+product_return_frequency = df_ret %>% 
+  filter(., returned == 'Yes') %>% 
+  group_by(., product_id) %>% 
+  summarise(., times_returned = n())
+
+# merge on product_id
+df_ret = merge(df_ret, product_return_frequency, by='product_id', all = TRUE)
+# make NAs 0
+df_ret$times_returned[is.na(df_ret$times_returned)] = 0
+
+write.csv(df_ret, 'lab2.csv', row.names = FALSE)
